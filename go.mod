afe7dd7f2f573360e131a7e8e97a8af01fc48ff5
Curtis Knudson <curtis@midgard.marketing>
2023-04-30 21:01:44 -0600

    Create small cli app for chat gpt in command line

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/curtisknudson/go-gpt
+
+go 1.19
+
+require github.com/sashabaranov/go-openai v1.9.1
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func MustGetenv(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		log.Fatalf("%s environment variable not set.", k)
+	}
+	return v
+}
+
+func main() {
+
+	var KEY = MustGetenv("CHATGPT")
+
+	fmt.Print(KEY)
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: chat <message>")
+		return
+	}
+
+	// Get the message from the command-line argument
+	message := os.Args[1]
+
+	client := openai.NewClient(KEY)
+
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: message,
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+}
+
+func MustGetEnv(s string) {
+	panic("unimplemented")
+}
